0478be7f671194fd72eabb61b6d1cde47358e0c6
erhic <eric.gichovi@student.moringaschool.com>
2021-12-01 17:23:07 +0300

    add function to store data from local storange

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
-$(document).ready(function() {
-    $('.carousel').carousel();
-});
-
-
-// object to mutate
-class pizzaBlueprint {
+class objPizza {
     constructor(size, topping, crust, price, quantity) {
         this.size = size;
         this.topping = topping;
@@ -14,77 +8,50 @@ class pizzaBlueprint {
     }
 }
 
-// an array to holde details for cart
 const cart = [];
 
-//function to run on click place order button,
-function placeToCart(pizzaSize, topping, crust, price, pizzaQuantity) {
-
-    // new instance object converting  quantity to number
-    pizza = new pizaDish(pizzaSize, "pineapple", "stuffed", price, parseInt(pizzaQuantity));
-
-    // adding more element in the array declared above
-    cart.push(pizza);
-
-
-    let cartCount = 0;
-
-
-    cart.forEach(pizzaObject => {
-        cartItem = cartItem + pizzaObject.quantity;
-    });
-
-    document.getElementById("thebadge").innerText = cartItem;
-
-
+// function on Refresh to load  data in the local storage value
+function onLoadCartItems() {
+    let cartItemscount = localStorage.getItem('cartItemscount');
+    if (cartItemscount) {
+        document.querySelector('#cartlabel').textContent = cartItemscount;
+    }
 }
 
-function AddToCart() {
-    pizzaSize = document.getElementById("pizzaSize").value;
-    pizzaQuantity = document.getElementById("quantityReq").value;
-
-
-    let priceOnSize = 600;
-
-    if (sizeOfPizza == "small") { priceOnSize = 400; }
-    elseif(sizeOfPizza == "large") { priceOnSize = 500; }
-    else(sizeOfPizza == "medium") { priceOnSize = 600; }
 
 
 
-    let priceOfTopping = 300;
 
-    if (topOfPizza == "Prosciutto") { priceOfTopping = 300; }
-    elseif(topOfPizza == "Garlic_butter") { priceOfTopping = 350; }
-    else(topOfPizza == "Pepperoni") { priceOfTopping = 500; }
-
-
-
-    let priceOfCrust = 400;
-
-
-    if (crustOfPizza == "gluttedfree") { priceOfCrust = 400; }
-    elseif(crustOfPizza == "crimpsy") { priceOfCrust = 550; }
-    else(crustOfPizza == "stuffed") { priceOfCrust = 650; }
-
-
-    // assigning price based on condition above
-    price = priceOnSize + priceOfTopping + priceOfCrust;
-
-    pizza = new pizaDish(pizzaSize, "pineapple", "stuffed", price, parseInt(pizzaQuantity));
+function placeToCart(pizzaSize, pizzatoppings, pizzacrust, price, pizzaQuantity) {
+    pizza = new objPizza("pizzaSize", "pizzatoppings", "pizzacrust", parseInt(price), parseInt(pizzaQuantity));
 
     // Add pizza object to array cart.
     cart.push(pizza);
 
-
+    //   get piza quantity
     let cartItemscount = 0;
     // Loop each pizza object
     cart.forEach(pizzaObject => {
-        cartItem += pizzaObject.quantity;
-
+        cartItemscount += pizzaObject.quantity;
     });
 
-    document.getElementById("thebadge").innerText = cartItem;
+    document.getElementById("cartlabel").innerText = cartItemscount;
+
+    let productNumbers = localStorage.getItem('cartItemscount');
 
 
+    productNumbers = parseInt(cartItemscount);
+
+    if (cartItemscount) {
+
+        localStorage.setItem('cartItemscount', cartItemscount + 0);
+        // document.getElementById("#cartlabel").textContent = cartItemscount + 1;
+    } else {
+        localStorage.setItem('cartItemscount', 0);
+        // document.getElementById("#cartlabel").textContent = cartItemscount;
+
+    }
+    console.log('cartItemscount');
+    console.log(typeof cartItemscount)
 }
+onLoadCartItems();
